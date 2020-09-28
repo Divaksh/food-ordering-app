614a33568e9f26a642878d7489666ec274140e0c
shank0020 <shashank.cse20@gmail.com>
2020-09-28 18:15:59 +0530

    implemented restaurantsByName method in RestaurantService

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -2,8 +2,10 @@ package com.upgrad.FoodOrderingApp.service.businness;
 
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantService {
@@ -15,6 +17,25 @@ public class RestaurantService {
     public List<RestaurantEntity> restaurantsByRating() {
         List<RestaurantEntity> restaurantEntities = restaurantDao.getAllRestaurantsByRating();
         return restaurantEntities;
+    }
+    //This method checks for the restaurant search field if its empty it throws corresponding exception
+    //It also returns the restaurants even if there is partial match in the restaurant in DB and the resto. mentioned in search field
+
+    public List<RestaurantEntity> restaurantsByName(final String restaurantName) throws RestaurantNotFoundException {
+        if (restaurantName.isEmpty()) {
+            throw new RestaurantNotFoundException("RNF-003", "Restaurant name field should not be empty");
+        }
+
+        List<RestaurantEntity> restaurantListByRating = restaurantDao.getAllRestaurantsByRating();
+        List<RestaurantEntity> matchingRestaurantList = new ArrayList<>();
+
+        for (RestaurantEntity restaurantEntity : restaurantListByRating) {
+            if (restaurantEntity.getRestaurantName().toLowerCase().contains(restaurantName.toLowerCase())) {
+                matchingRestaurantList.add(restaurantEntity);
+            }
+        }
+
+        return matchingRestaurantList;
     }
 
 }
