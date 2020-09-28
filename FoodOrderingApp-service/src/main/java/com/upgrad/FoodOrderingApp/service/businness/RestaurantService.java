7e270784e396a295420ccfa2d27a474f1f06f70d
shank0020 <shashank.cse20@gmail.com>
2020-09-28 17:48:54 +0530

    add getAllRestaurent method in RestaurentService class to interact with the dao layer

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -1,11 +1,15 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class RestaurantService {
 
+    @Autowired
+    private RestaurantDao restaurantDao;
 
     //This method returns all the restaurants according to the customer ratings
     public List<RestaurantEntity> restaurantsByRating() {
