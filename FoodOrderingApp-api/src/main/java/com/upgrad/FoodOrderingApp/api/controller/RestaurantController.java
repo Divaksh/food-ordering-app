10f4e41eb401a446ae8348114b6b65e8887aa6f8
shank0020 <shashank.cse20@gmail.com>
2020-09-28 15:29:30 +0530

    commented all the test cases initially

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
@@ -1,8 +1,28 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+import java.util.List;
+
 @RestController
+@RequestMapping("/")
 public class RestaurantController {
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET, path = "/restaurant", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public void getAllRestaurants(){
+        List<RestaurantEntity> restaurantEntities = restaurantService.restaurantsByRating();
+    }
 
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/RestaurantEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/RestaurantEntity.java
@@ -1,4 +1,48 @@
 package com.upgrad.FoodOrderingApp.service.entity;
 
-public class RestaurantEntity {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="restaurant")
+@NamedQueries({
+        @NamedQuery(name = "getAllRestaurantsByRating", query = "select q from RestaurantEntity q order by q.customerRating desc"),
+})
+public class RestaurantEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="uuid")
+    @Size(max=200)
+    @NotNull
+    private String uuid;
+
+    @Column(name="restaurant_name")
+    @Size(max=30)
+    @NotNull
+    private String restaurantName;
+
+    @Column(name="photo_url")
+    @Size(max=255)
+    @NotNull
+    private String photoUrl;
+
+    @Column(name="customer_rating")
+    @NotNull
+    private BigDecimal customerRating;
+
+    @Column(name="average_price_for_two")
+    @NotNull
+    private Integer averagePriceForTwo;
+
+    @Column(name="number_of_customers_rated")
+    @NotNull
+    private Integer numberOfCustomersRated;
+
+
 }
