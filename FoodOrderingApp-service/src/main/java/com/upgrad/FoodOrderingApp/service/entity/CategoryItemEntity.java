704c0279b3ae8e8c4de38f854a710819f1d8c70c
shank0020 <shashank.cse20@gmail.com>
2020-09-28 20:23:58 +0530

    implemented CategoryItemEntity as per db

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CategoryItemEntity.java
@@ -0,0 +1,50 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="category_item")
+public class CategoryItemEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="item_id")
+    private ItemEntity itemId;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="category_id")
+    private CategoryEntity categoryId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ItemEntity getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(ItemEntity itemId) {
+        this.itemId = itemId;
+    }
+
+    public CategoryEntity getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(CategoryEntity categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public CategoryItemEntity() {
+    }
+}
+
+
