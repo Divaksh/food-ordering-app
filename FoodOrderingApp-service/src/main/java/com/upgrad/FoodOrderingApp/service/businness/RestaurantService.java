5645d2c8a762aa511722e9b0edc423d7d81553a8
shank0020 <shashank.cse20@gmail.com>
2020-09-28 15:28:40 +0530

    restaurantsByRating method added to RestaurantService class to retrieve the list of all the restaurent in the db

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -1,4 +1,15 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+
+import java.util.List;
+
 public class RestaurantService {
+
+
+    public List<RestaurantEntity> restaurantsByRating() {
+        List<RestaurantEntity> restaurantEntities = restaurantDao.getAllRestaurantsByRating();
+        return restaurantEntities;
+    }
+
 }
