a20259e665f82bc9e05bd14ba68d50ab2d6d2371
shank0020 <shashank.cse20@gmail.com>
2020-10-04 23:15:35 +0530

    implemented getCategoryById method under CategoryController class

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
@@ -1,14 +1,19 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
 import com.upgrad.FoodOrderingApp.api.model.CategoriesListResponse;
+import com.upgrad.FoodOrderingApp.api.model.CategoryDetailsResponse;
 import com.upgrad.FoodOrderingApp.api.model.CategoryListResponse;
+import com.upgrad.FoodOrderingApp.api.model.ItemList;
 import com.upgrad.FoodOrderingApp.service.businness.CategoryService;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import com.upgrad.FoodOrderingApp.service.entity.ItemEntity;
+import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -39,5 +44,32 @@ public class CategoryController {
             categoriesListResponse.addCategoriesItem(categoryListResponse);
         }
         return new ResponseEntity<CategoriesListResponse>(categoriesListResponse, HttpStatus.OK);
+    }
+
+    //This method is retrieves category based on the input category Id
+    //And generates response object along with appropriate status code
+    //And throws CategoryNotFoundExceptions in cases where category Id is empty and there are no categories available by the id provided
+
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET, path = "/category/{category_id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<CategoryDetailsResponse> getCategoryById(
+            @PathVariable("category_id") final String categoryId) throws CategoryNotFoundException {
+
+        CategoryEntity categoryEntity = categoryService.getCategoryById(categoryId);
+
+        CategoryDetailsResponse categoryDetailsResponse = new CategoryDetailsResponse()
+                .id(UUID.fromString(categoryEntity.getUuid()))
+                .categoryName(categoryEntity.getCategoryName());
+
+        for (ItemEntity itemEntity : categoryEntity.getItems()) {
+
+            ItemList itemList = new ItemList()
+                    .id(UUID.fromString(itemEntity.getUuid()))
+                    .itemName(itemEntity.getItemName())
+                    .price(itemEntity.getPrice())
+                    .itemType(ItemList.ItemTypeEnum.fromValue(itemEntity.getType().getValue()));
+            categoryDetailsResponse.addItemListItem(itemList);
+        }
+        return new ResponseEntity<CategoryDetailsResponse>(categoryDetailsResponse, HttpStatus.OK);
     }
 }
